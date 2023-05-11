1ef496bd7aa1b92419d21f49605336759ac41089
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-05-11 14:21:00 +0300

    update

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Start server port 3000!');
+app.get('/test', (req, res, next) => {
+  res.send('Hello World!');
 });
+
+app.get('/contacts/1', (req, res, next) => {
+  res.json({ name: 'Petro', age: 30 });
+});
+
+module.exports = app;
